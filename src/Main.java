3749ade9b80b526c8c48176a995b5f19cd7c121e
G-EssarySchool <essary.619432@swl.k12.oh.us>
2025-10-29 08:42:49 -0400

    this

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (String[] args) {
+        Rectangle r1 = new Rectangle(3, 4);
+        System.out.println("r1 Info: ");
+        r1.printRectangle();
+    }
+}
