7c8a1553fe4ec96523efdc53c15d4e0980daac3d
G-EssarySchool <essary.619432@swl.k12.oh.us>
2025-11-04 08:21:03 -0500

    Task 1 complete

---
+++ src/Task1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String username = InputHelper.getNonZeroLengthString(scan, "Please enter your username:");
+        System.out.println("The username you entered is: " + username);
+    }
+}
