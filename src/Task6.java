5a28d9b8911473f7013075a62ab1c76f433b4e4f
G-Essary <essary.619432@swl.k12.oh.us>
2025-11-06 20:39:19 -0500

    Finished Task 6

--- src/Task6.java
+++ src/Task6.java
@@ -2,8 +2,16 @@ import java.util.Scanner;
 
 public class Task6 {
 
-    //celsiusToFahrenheit method: takes Celsius temperature (double) and returns the equivalent Fahrenheit temperature as a double.
-    public static double celsiusToFahrenheit(double Celsius) {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        double celsius = InputHelper.getDouble(scan, "Enter temperature in Celsius: ");
+
+        double fahrenheit = celsiusToFahrenheit(celsius);
+
+        System.out.printf("Temperature in Fahrenheit: %.2f Fahrenheit%n", fahrenheit);
+    }
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9.0 / 5.0) + 32.0;
     }
 }
