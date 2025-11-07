b85f8b7ea479f4ec818ee0de1db61972a41c8be1
G-Essary <essary.619432@swl.k12.oh.us>
2025-11-06 20:40:09 -0500

    Polishing Tasks

--- src/Task1.java
+++ src/Task1.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
         String username = InputHelper.getNonZeroLengthString(scan, "Please enter your username:");
+        
         System.out.println("The username you entered is: " + username);
     }
 }
